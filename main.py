aa0638c4d7569f7e657a031693e161b2fd4a0ae5
mukhlisagithub <121219461+mukhlisagithub@users.noreply.github.com>
2023-02-05 19:55:21 +0500

    idea remove qilindi

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from telegram.ext import Updater
 
 updater = Updater
+# git add . --> yaratilgan proyectlarni gitga qoshsin degan buyruq!
+# git commit -m ""  --> nmani ozgarganini aytib qoyish qoshtirnoq ichiga "main va requirements fayllar yaratildi"
+# git push --> git hubga yuborib qoyish qilingan loyihalarni
+
+# .gitignore --> telegram bot yaratilganda yokida bironta websayt yaratilganda ichidagi tokinla githubga chiqib ketmasligi kerak shu fayl ichida turishi kerak
